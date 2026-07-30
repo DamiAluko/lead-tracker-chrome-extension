8bbea54f92c2872a29e6b1015081d8c9d28aa879
DamiAluko <damilolaaluko.da@gmail.com>
2026-07-30 17:15:54 +0100

    Links made clickable, and security fix added

--- index.js
+++ index.js
@@ -6,16 +6,27 @@ let myLeads = []
 
 function saveLead(){
   myLeads.push(inputEl.value)
+  renderLead()
   inputEl.value = ""
-  
-  let allListItems = ""
-  for(let i = 0; i < myLeads.length; i++){
-    allListItems += "<li>" + myLeads[i] + "</li>"
-  }
-  ulistEl.innerHTML = allListItems
-
-  console.log(myLeads)
-  console.log(allListItems)
 }
 
 saveInput.addEventListener('click', saveLead)
+
+function renderLead(){
+  //let listItem = "<li><a href='" + inputEl.value + "' target='_blank' rel='noopener noreferrer'>" + inputEl.value + "</a>" + "</li>";
+  let listItem = `
+    <li>
+      <a href= ${inputEl.value} 
+      target='_blank' 
+      rel='noopener noreferrer'>
+        ${inputEl.value} 
+      </a>  
+    </li>`;
+  
+  ulistEl.innerHTML += listItem
+}
+// NOTE: it's good practice to also include rel="noopener noreferrer". 
+// Without it, the newly opened page can access window.opener and potentially 
+// redirect your original tab (a known security/phishing risk), 
+// and in some older browsers it can also cause a minor performance hit since 
+// the new page shares the same process.
